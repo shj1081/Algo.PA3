eeed3c76b499025ee5fcfa1579a741a618dc303a
Hjoon_sub <shj83811081@gmail.com>
2023-11-28 23:09:15 +0900

    add annotation

--- second.c
+++ second.c
@@ -3,90 +3,127 @@
 #include <string.h>
 #include <time.h>
 
-#define MAX_OPER_NUM 10000
-#define MAX_NODE_NUM 500
-#define INT_MAX 2147483647
+#define MAX_OPER_NUM 10000 /**< The maximum number of operations */
+#define MAX_NODE_NUM 500   /**< The maximum number of nodes */
+#define INT_MAX 2147483647 /**< Represent infinity in key value */
 
 typedef struct vertex {
-  int name;
-  int key;
-  struct vertex *next;
+  int name;             // name of the vertex
+  int key;              // key value used in priority queue
+  struct vertex *next;  // pointer to the next vertex in the linked list
 } vertex;
 
 typedef struct priorityQueue {
-  int size;
-  vertex *arr[MAX_NODE_NUM];
+  int size;                        // size of the priority queue
+  vertex *arr[MAX_NODE_NUM];       // array of vertices in the priority queue
   int position[MAX_NODE_NUM + 1];  // index is the name of vertex, value is the
                                    // index of vertex in arr
 } priorityQueue;
 
+/**
+ * Initializes the priority queue with the given number of nodes.
+ */
 void initQueue(priorityQueue *pq, int nodeNum) {
   pq->size = nodeNum;
   for (int i = 0; i < nodeNum; i++) {
     pq->arr[i] = (vertex *)malloc(sizeof(vertex));
-    pq->arr[i]->name = i + 1;
-    pq->arr[i]->key = INT_MAX;
-    pq->position[i + 1] = i;
+    pq->arr[i]->name = i + 1;   // set name of vertex
+    pq->arr[i]->key = INT_MAX;  // set key to infinity
+    pq->position[i + 1] = i;    // reset position of vertex
   }
 }
 
+/**
+ * Initializes the vertex linked list by setting all elements to NULL.
+ */
 void initVertexLinkedList(vertex *vertexLinkedList[], int nodeNum) {
   for (int i = 1; i <= nodeNum; i++) {
     vertexLinkedList[i] = NULL;
   }
 }
 
+/**
+ * Swaps the values of two vertex pointers.
+ */
 void swap(vertex **a, vertex **b) {
   vertex *temp = *a;
   *a = *b;
   *b = temp;
 }
 
+/**
+ * Performs the heapify up operation on the priority queue.
+ */
 void heapifyUp(priorityQueue *pq, int index) {
   if (index == 0) {
     return;
   }
   int parent = (index - 1) / 2;
+
+  // if parent is greater than child, swap them
   if (pq->arr[parent]->key > pq->arr[index]->key) {
     swap(&pq->arr[parent], &pq->arr[index]);
+
+    // update position of vertices
     pq->position[pq->arr[parent]->name] = parent;
     pq->position[pq->arr[index]->name] = index;
     heapifyUp(pq, parent);
   }
 }
 
+/**
+ * Performs the heapify down operation on the priority queue.
+ */
 void heapifyDown(priorityQueue *pq, int index) {
   int left = index * 2 + 1;
   int right = index * 2 + 2;
   int smallest = index;
+
+  // find the smallest child
   if (left < pq->size && pq->arr[left]->key < pq->arr[smallest]->key) {
     smallest = left;
   }
   if (right < pq->size && pq->arr[right]->key < pq->arr[smallest]->key) {
     smallest = right;
   }
+
+  // if smallest child is not the parent, swap them
   if (smallest != index) {
     swap(&pq->arr[smallest], &pq->arr[index]);
+
+    // update position of vertices
     pq->position[pq->arr[smallest]->name] = smallest;
     pq->position[pq->arr[index]->name] = index;
     heapifyDown(pq, smallest);
   }
 }
 
+/**
+ * Extracts the minimum element from the priority queue.
+ */
 vertex *extractMin(priorityQueue *pq) {
+  // if priority queue is empty, return NULL
   if (pq->size == 0) {
     return NULL;
   }
   vertex *min = pq->arr[0];
   pq->arr[0] = pq->arr[pq->size - 1];
+
+  // update position of vertices
   pq->position[pq->arr[0]->name] = 0;
+
   pq->size--;
   heapifyDown(pq, 0);
   return min;
 }
 
+/**
+ * Inserts an edge into the graph.
+ */
 void insertEdge(int *weight[], int *numbers, vertex *vertexLinkedList[]) {
+  // if edge does not exist, insert it
   if (weight[numbers[0]][numbers[1]] == 0) {
+    // set weight of edge in weight matrix
     weight[numbers[0]][numbers[1]] = numbers[2];
     weight[numbers[1]][numbers[0]] = numbers[2];
 
@@ -120,8 +157,13 @@ void insertEdge(int *weight[], int *numbers, vertex *vertexLinkedList[]) {
   }
 }
 
+/**
+ * Deletes an edge from the graph.
+ */
 void deleteEdge(int *weight[], int *numbers, vertex *vertexLinkedList[]) {
+  // if edge exists, delete it
   if (weight[numbers[0]][numbers[1]] != 0) {
+    // set weight of edge in weight matrix
     weight[numbers[0]][numbers[1]] = 0;
     weight[numbers[1]][numbers[0]] = 0;
 
@@ -161,8 +203,13 @@ void deleteEdge(int *weight[], int *numbers, vertex *vertexLinkedList[]) {
   }
 }
 
+/**
+ * Changes the weight of an edge in the graph.
+ */
 void changeWeight(int *weight[], int *numbers, vertex *vertexLinkedList[]) {
+  // if edge exists, change its weight
   if (weight[numbers[0]][numbers[1]] != 0) {
+    // set weight of edge in weight matrix
     weight[numbers[0]][numbers[1]] = numbers[2];
     weight[numbers[1]][numbers[0]] = numbers[2];
 
@@ -188,15 +235,21 @@ void changeWeight(int *weight[], int *numbers, vertex *vertexLinkedList[]) {
   }
 }
 
+/**
+ * Finds the minimum spanning tree of the graph using Prim's algorithm.
+ */
 void findMST(priorityQueue *pq, int *weight[], int nodeNum,
              vertex *vertexLinkedList[], FILE *out) {
   int MST = 0;
   pq->arr[0]->key = 0;
+
+  // array to check if the vertex is connected
   int connected[nodeNum];
   for (int i = 0; i < nodeNum; i++) {
     connected[i] = 0;
   }
 
+  // check if the graph is disconnected
   while (pq->size != 0) {
     vertex *u = extractMin(pq);
     connected[u->name - 1] = 1;
@@ -218,6 +271,7 @@ void findMST(priorityQueue *pq, int *weight[], int nodeNum,
       temp = temp->next;
     }
   }
+  // write MST to output file
   fprintf(out, "%d\n", MST);
 }
 
@@ -225,12 +279,15 @@ int main() {
   int TIME = 0;
   clock_t start = clock();
 
+  // open input and output files
   FILE *file = fopen("mst.in", "r");
   FILE *out = fopen("mst.out", "w");
 
+  // read the number of nodes
   int nodeNum = 0;
   fscanf(file, "%d", &nodeNum);
 
+  // initialize weight matrix
   int **weight = (int **)malloc(sizeof(int *) * (nodeNum + 1));
   for (int i = 0; i <= nodeNum; i++) {
     weight[i] = (int *)malloc(sizeof(int) * (nodeNum + 1));
@@ -239,15 +296,18 @@ int main() {
     }
   }
 
+  // initialize priority queue
   priorityQueue pq;
   initQueue(&pq, nodeNum);
 
+  //  initialize vertex linked list
   vertex *vertexLinkedList[MAX_NODE_NUM + 1];
   initVertexLinkedList(vertexLinkedList, nodeNum);
 
   char oper[15];  // for storing operation temporarily
-  int numbers[3];
+  int numbers[3]; // for storing numbers temporarily
 
+  // read operations from input file and perform them
   while (fscanf(file, "%s", oper) != EOF) {
     if (strcmp(oper, "insertEdge") == 0) {
       fscanf(file, "%d %d %d", &numbers[0], &numbers[1], &numbers[2]);
@@ -259,11 +319,13 @@ int main() {
       fscanf(file, "%d %d", &numbers[0], &numbers[1]);
       deleteEdge(weight, numbers, vertexLinkedList);
     } else if (strcmp(oper, "findMST") == 0) {
-      initQueue(&pq, nodeNum);
+      initQueue(&pq,
+                nodeNum);  // reset priority queue for each findMST operation
       findMST(&pq, weight, nodeNum, vertexLinkedList, out);
     }
   }
 
+  // close input and output files
   fclose(file);
   fclose(out);
 
